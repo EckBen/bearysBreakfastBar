588dc51f9e3fcb1fef20c6c41285bf28c35b2de6
Ben <benlinux915@gmail.com>
2021-01-22 08:11:43 -0500

    fixed button variable error

--- src/index.js
+++ src/index.js
@@ -11,15 +11,15 @@ loadMain(currentTab, content, reviews, homeTitle, info);
 
 // Set click events for tab navigation
 btnNames.forEach(btnName => {
-  let btn = document.getElementById(btnName);
+  var btn = document.getElementById(btnName);
   btn.addEventListener('click', () => {
-    currentTab = changeBtnCover(currentTab, btnName);
-    if (btnName == 'home') {
-      loadMain(btnName, content, reviews, homeTitle, info);
-    } else if (btnName == 'menu') {
-      loadMain(btnName, content, menu, menuTitle);
+    currentTab = changeBtnCover(currentTab, btn.id);
+    if (btn.id == 'home') {
+      loadMain(btn.id, content, reviews, homeTitle, info);
+    } else if (btn.id == 'menu') {
+      loadMain(btn.id, content, menu, menuTitle);
     } else {
-      loadMain(btnName, content, contacts, contactTitle);
+      loadMain(btn.id, content, contacts, contactTitle);
     }
   });
 })
